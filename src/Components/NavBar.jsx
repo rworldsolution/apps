b30ecff65e74977a8a0ffbd00f361c372ffd370a
EDUNEXT <rishav.edunext@gmail.com>
2025-01-30 15:20:45 +0530

    ch

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,44 +1,112 @@
-import React from 'react'
-// import { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import icon1 from '../assets/img/dashboard-icon.svg'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import icon1 from '../assets/img/dashboard-icon.svg';
+import icon2 from '../assets/img/Purchase.svg';
+import icon3 from '../assets/img/Sale.svg';
+import icon4 from '../assets/img/Users.svg';
+import icon5 from '../assets/img/Report.svg';
+import icon6 from '../assets/img/Configuration.svg';
+
 const navList = [
   {
     id: 1,
     name: 'Dashboard',
     url: '/',
-    img: icon1
+    img: icon1,
   },
   {
     id: 2,
-    name: 'Item',
-    url: '/Item',
-    img: icon1
+    name: 'Inventory',
+    url: '/Inventory',
+    img: icon2,
+    subItems: [
+      { id: 10, name: 'Item', url: '/Item' },
+      { id: 11, name: 'Stock Adjustment', url: '/StockAdjustment' },
+      { id: 12, name: 'Stock Alert', url: '/StockAlert' },
+    ],
   },
   {
     id: 3,
-    name: 'ItemAdd',
-    url: '/ItemAdd',
-    img: icon1
+    name: 'Purchase',
+    url: '/Purchase',
+    img: icon3,
+    subItems: [
+      { id: 13, name: 'Vendor', url: '/Vendor' },
+      { id: 14, name: 'Purchase Order', url: '/PurchaseOrder' },
+      { id: 15, name: 'Purchase Invoice', url: '/PurchaseInvoice' },
+    ],
   },
   {
     id: 4,
-    name: 'ItemPreview',
-    url: '/ItemPreview',
-    img: icon1
-  }
-]
+    name: 'Sale',
+    url: '/Sale',
+    img: icon3,
+    subItems: [
+      { id: 18, name: 'Customer', url: '/Customer' },
+      { id: 19, name: 'SalesOrder', url: '/SalesOrder' },
+      { id: 19, name: 'SalesInvoice', url: '/SalesInvoice' },
+      { id: 19, name: 'PaymentReceived', url: '/PaymentReceived' },
+      { id: 19, name: 'Shipment', url: '/Shipment' },
+      { id: 19, name: 'Packages', url: '/Packages' },
+      { id: 19, name: 'SaleReturn', url: '/SaleReturn' },
+      { id: 19, name: 'CreditNote', url: '/CreditNote' }
+    ],
+  },
+  {
+    id: 5,
+    name: 'Users',
+    url: '/Users',
+    img: icon4,
+  },
+  {
+    id: 6,
+    name: 'Report',
+    url: '/Report',
+    img: icon5,
+  },
+  {
+    id: 7,
+    name: 'Configuration',
+    url: '/Configuration',
+    img: icon6,
+  },
+];
+
 export default function NavBar() {
+  const [activeMenu, setActiveMenu] = useState(null);
+
+  const toggleSubMenu = (menuId) => {
+    setActiveMenu((prevMenu) => (prevMenu === menuId ? null : menuId));
+  };
+
   return (
     <nav>
-      {navList.map(nav => {
-        return (
+      <ul>
+        {navList.map((nav) => (
           <li key={nav.id}>
-            {/* <Link to={nav.url}>{nav.name}</Link> */}
-            <NavLink className={({isActive}) => isActive ? 'active' : ''} to={nav.url}><img src={nav.img} /> {nav.name}</NavLink>
+            <div
+              onClick={() => toggleSubMenu(nav.id)}
+              className={`menu-item ${nav.id}`}
+            >
+              <NavLink to={nav.url} className={({ isActive }) => (isActive ? 'active' : '')}>
+                <img src={nav.img} alt={nav.name} /> {nav.name}
+              </NavLink>
+            </div>
+
+            {nav.subItems && (
+              <ul className={`sub-menu ${activeMenu === nav.id ? 'show' : ''}`}>
+                {nav.subItems.map((sub) => (
+                  <li key={sub.id}>
+                    <NavLink to={sub.url} className={({ isActive }) => (isActive ? 'active' : '')}>
+                      {sub.name}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
-        )
-      })}
+        ))}
+      </ul>
     </nav>
-  )
+  );
 }
